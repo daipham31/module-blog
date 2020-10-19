d38a4a5d614f66c71207d526b9b6b6ac21cd4ea1
DaiPham <daipham31@outlook.com>
2020-10-19 14:27:30 +0700

    Module Creation

---
+++ BlogSimple/Model/ResourceModel/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Duud\BlogSimple\Model\ResourceModel;
+
+use \Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
+class Post extends AbstractDb
+{
+    /**
+     * Post Abstract Resource Constructor
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('blog_post', 'post_id');
+    }
+}
---
+++ BlogSimple/Model/ResourceModel/Post/Collection.php
@@ -0,0 +1,16 @@
+<?php
+namespace Duud\BlogSimple\Model\ResourceModel\Post;
+
+use \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
+class Collection extends AbstractCollection
+{
+    /**
+     * Remittance File Collection Constructor
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Duud\BlogSimple\Model\Post', 'Duud\BlogSimple\Model\ResourceModel\Post');
+    }
+}
