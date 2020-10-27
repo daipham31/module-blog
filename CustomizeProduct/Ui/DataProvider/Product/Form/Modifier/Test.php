f86cb6cb1b2f7f0bdf5decd3558a847b8da5f4f2
DaiPham <daipham31@outlook.com>
2020-10-27 17:47:10 +0700

    Customize Product

---
+++ CustomizeProduct/Ui/DataProvider/Product/Form/Modifier/Test.php
@@ -0,0 +1,34 @@
+<?php
+namespace Duud\CustomizeProduct\Ui\DataProvider\Product\Form\Modifier;
+
+use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\AbstractModifier;
+use Magento\Ui\Component\Form\Field;
+use Magento\Ui\Component\Form\Fieldset;
+
+class Test extends AbstractModifier
+{
+    public function modifyMeta(array $meta)
+    {
+        $meta['stock_data'] = [
+            'arguments' => [
+                'data' => [
+                    'config' => [
+                        'label' => __('Manage Stock'),
+                        'sortOrder' => 50,
+                        'collapsible' => true
+                    ]
+                ]
+            ]
+        ];
+
+ return $meta;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function modifyData(array $data)
+    {
+        return $data;
+    }
+}
